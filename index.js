f5a0e7ff49ca82d4f15cecbc847026cf289efeca
mmshareef <muhammadmuadassirshareef@gmail.com>
2020-03-17 14:54:34 +0530

    faker.js

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ var Faker = require('./lib');
 var faker = new Faker({ locales: require('./lib/locales') });
 module['exports'] = faker;
 console.log(faker.food.indian());
+console.log(faker.food.italian());
+console.log(faker.food.chinese());
+console.log(faker.food.continental());
